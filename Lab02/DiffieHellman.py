b8f9cd8aa941385abf898f6afcac40c7d0f6b768
Harris Ransom <hransom528@gmail.com>
2025-04-08 12:13:08 -0400

    Lab02 finished Diffie Hellman

--- Lab02/DiffieHellman.py
+++ Lab02/DiffieHellman.py
@@ -3,13 +3,23 @@
 # Part 6: Secure Key Exchange using Diffie-Hellman
 
 # Imports
-import math
+import secrets
+import sympy
 import random
+
+# Generate a random prime number between a lower and upper bound
+# See: https://stackoverflow.com/questions/21043075/generating-large-prime-numbers-in-python
+def rand_prime(bits):
+    while True:
+        p = secrets.randbits(bits)
+        if (sympy.isprime(p)):
+            break
+    return p
 
 # MAIN
 if __name__ == "__main__":
-    # TODO: Large prime number
-    #p = 
+    # Generate large prime number
+    p = rand_prime(256)
 
     # Secret values
     a = random.randrange(3, 101+1, 2)
@@ -27,7 +37,9 @@ if __name__ == "__main__":
     secretB = pow(A, b) % p
     if (secretA == secretB):
         print("Shared secret established successfully!")
+        print("Shared secret: ", secretA)
     else:
         print("Shared secret establishment failed!")
     print("Alice's secret: ", a)
     print("Bob's secret: ", b)
+    
